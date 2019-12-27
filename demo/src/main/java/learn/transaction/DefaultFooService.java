aa11dc1255ed9789185b47f0146bc1218d1270cd
kaka2634 <996529090@qq.com>
2019-12-27 17:35:26 +0800

    refactor and add learn transaction

--- demo/src/main/java/learn/transaction/DefaultFooService.java
+++ demo/src/main/java/learn/transaction/DefaultFooService.java
@@ -3,25 +3,29 @@ package learn.transaction;
 
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
+//@Transactional
 public class DefaultFooService implements FooService{
     @Override
     public Foo getFoo(String fooName) {
+        System.out.println("getFoo(String) been called");
         throw new UnsupportedOperationException();
     }
 
     @Override
     public Foo getFoo(String fooName, String barName) {
+        System.out.println("getFoo(String, String) been called");
         throw new UnsupportedOperationException();
     }
 
     @Override
     public void insertFoo(Foo foo) {
+        System.out.println("insertFoo been called");
         throw new UnsupportedOperationException();
     }
 
     @Override
     public void updateFoo(Foo foo) {
+        System.out.println("updateFoo been called");
         throw new UnsupportedOperationException();
     }
 }
